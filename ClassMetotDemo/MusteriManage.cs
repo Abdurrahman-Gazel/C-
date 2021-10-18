5e93fdf8801a189fba0c1fe42a3e6ebc40d4ef4b
Abdurrahman Gazel <173311078@ogr.selcuk.edu.tr>
2021-10-18 12:03:30 +0300

    2.

---
+++ ClassMetotDemo/MusteriManage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManage
+    {
+        public MusteriManage()
+        {
+
+        }
+        public void Ekle(Musteri gelenMusteri)
+        {
+            Console.WriteLine(gelenMusteri.Adi + " " + gelenMusteri.Soyad + " isimli musterimiz listeye eklenmistir");
+        }
+        public void Cikar(Musteri gelenMusteri)
+        {
+            Console.WriteLine(gelenMusteri.Adi + " " + gelenMusteri.Soyad + " isimli musterimiz listeye Cikarilmistir");
+        }
+        public void Guncelle()
+        {
+            Console.WriteLine("Liste guncellemesi basariyla gerceklesti");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "ahmet";
+            musteri1.Soyad = "bekir";
+            musteri1.TelefonNo = 0534343434;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "ahmet";
+            musteri2.Soyad = "bekir";
+            musteri2.TelefonNo = 0534343434;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "ahmet";
+            musteri3.Soyad = "bekir";
+            musteri3.TelefonNo = 0534343434;
+
+
+            MusteriManage musteriManage = new MusteriManage();
+            musteriManage.Ekle(musteri1);
+            musteriManage.Ekle(musteri2);
+            musteriManage.Ekle(musteri3);
+
+            musteriManage.Guncelle();
+
+            musteriManage.Cikar(musteri2);
+
+            musteriManage.Guncelle();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
